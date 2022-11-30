93a8ed4811d0c4be7eaf4cb8fbabf1d1ae7ded2a
robertvlad <vladrob96@gmail.com>
2022-11-30 21:27:46 +0100

    bonus 1 + bonus 2

--- js/script.js
+++ js/script.js
@@ -7,18 +7,27 @@
 // 3 - Creare un container nel DOM e mostare per ogni elemento il numero o la stringa associata
 // 4 - Definire un colore diverso per ongi tipologia di output
 
+const container = document.querySelector (".container")
+
 for (let i=1; i<=100; i++){
 
     if (i % 3 == 0 && i % 5 == 0){
+        let textInside = "FizzBuzz"
+        container.innerHTML += '<div class="square square-fizzbuzz">' + textInside + '</div>'
         console.log("FizzBuzz")
     }
     else if (i % 3 == 0){
+        let textInside = "Fizz"
+        container.innerHTML += '<div class="square square-fizz">' + textInside + '</div>'
         console.log("Fizz")
     }
     else if (i % 5 == 0){
+        let textInside = "Buzz"
+        container.innerHTML += '<div class="square square-buzz">' + textInside + '</div>'
         console.log("Buzz")
     }
     else{
+        container.innerHTML += '<div class="square square-normal">' + i + '</div>'
         console.log(i)
     }
 }
